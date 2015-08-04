cfa0485e4a6d98d7c5647f15cbbcccae5c707717
Andrew Brennwald & Kellen Kolbeck <student@epicodus.com>
2015-08-04 10:02:43 -0700

    Wrote first spec which determines the day of the week

---
+++ spec/day_of_the_week_spec.rb
@@ -0,0 +1,13 @@
+# Test spec for the day_of_the_week_method
+# Authors: Andrew Brennwald and Kellen Kolbeck
+
+require('rspec')
+require('day_of_the_week')
+
+# TEST 1: Tells what day of the week a given date is
+
+describe('Time#day_of_the_week') do
+  it ("Tells what day of the week it is given a certain date") do
+    expect(Time.new(1991,3,16).day_of_the_week()).to(eq(6))
+  end
+end
