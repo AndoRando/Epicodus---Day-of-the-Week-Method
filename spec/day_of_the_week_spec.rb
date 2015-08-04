905b4a530ee891ee7d95be182df274a9d4f234c7
Andrew Brennwald & Kellen Kolbeck <student@epicodus.com>
2015-08-04 10:10:29 -0700

    wrote spec for second test

--- spec/day_of_the_week_spec.rb
+++ spec/day_of_the_week_spec.rb
@@ -10,4 +10,10 @@ describe('Time#day_of_the_week') do
   it ("Tells what day of the week it is given a certain date") do
     expect(Time.new(1991,3,16).day_of_the_week()).to(eq(6))
   end
+
+# TEST 2: Tells the user they can sleep in on Saturday
+
+  it ("Tells the user they can sleep in on Saturday") do
+    expect(Time.new(1991,3,16).day_of_the_week()).to(eq("Go ahead an sleep in"))
+  end
 end
